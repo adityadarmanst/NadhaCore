b306c3c358cfc2939c8a20ee4e1a809a4e50cc40
Aditia Darma Nasution <38040029+adityadarmanst@users.noreply.github.com>
2018-05-06 05:04:50 +0700

    Update fastline.php

--- nadhaSys/views/kreator/fastline.php
+++ nadhaSys/views/kreator/fastline.php
@@ -6,6 +6,6 @@ $ambilFast = $link -> query("SELECT isi FROM tbl_fastline;");
 while($fFast = $ambilFast -> fetch_array()){
     
     $fastline = $fFast['isi'];
-    echo "<i class='fa fa-square'></i> ".$fastline.".&nbsp; &nbsp; ";
+    echo "<i class='fa fa-square'></i> ".$fastline.".&nbsp; &nbsp; <b>*</b>";
 }
 ?>
